ad0d457d4f368ff9f88be74ef6a29545bbb8a3af
Manuel Panichelli <manuel.panichelli@brubank.com>
2023-07-28 20:31:31 -0300

    parte 1 okayyyy

--- behavioural-types/assignment/file-server/FileServerThread.java
+++ behavioural-types/assignment/file-server/FileServerThread.java
@@ -10,36 +10,38 @@ public class FileServerThread extends Thread {
   public void run() {
     try {
       FileServer server = new FileServer();
-      if (server.start(socket)) {
-        System.out.println("File server started!");
-        if (!server.hasRequest()) {
-          server.close();
-          return;
-        }
-
-        String fileName = server.readFileName();
-        if (!server.fileExists(fileName)) {
-          server.sendFileEnd();
-          return;
-        }
-
-        byte[] fileContent = {
-          (byte) 0xDE,
-          (byte) 0xAD,
-          (byte) 0xBE,
-          (byte) 0xEF,
-        };
-
-        for (byte b : fileContent) {
-          server.sendByte(b);
-        }
-
-        server.sendFileEnd();
-      
-        server.close();
-      } else {
+      if (!server.start(socket)) {
         System.out.println("Could not start server!");
+        return;
       }
+
+      System.out.println("File server started!");
+      if (!server.hasRequest()) {
+        server.close();
+        return;
+      }
+
+      String fileName = server.readFileName();
+      if (!server.fileExists(fileName)) {
+        server.sendFileEnd();
+        server.close();
+        return;
+      }
+
+      byte[] fileContent = {
+        (byte) 0xDE,
+        (byte) 0xAD,
+        (byte) 0xBE,
+        (byte) 0xEF,
+      };
+
+      for (byte b : fileContent) {
+        server.sendByte(b);
+      }
+
+      server.sendFileEnd();
+    
+      server.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
